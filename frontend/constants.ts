097275e1bd30af86b6b8b111c4af43d34c6966fa
kazmiura2024-rgb <kazmiura2024@gmail.com>
2026-09-21 07:42:23 +0900

    Tic-Tac-ToeDeployVerifire

---
+++ frontend/constants.ts
@@ -0,0 +1,16 @@
+export const WINNING_COMBINATIONS = [
+  // 横
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  // 縦
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  // 斜め
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+export const APP_VERSION = 'v1.0.4-deploy-check';
+export const BUILD_TIMESTAMP = new Date().toISOString();
